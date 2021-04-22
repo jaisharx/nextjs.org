4a10a6e7ff3aea3e8e1f23fefeb7b608df2b88f5
jai-sharma <sendtojsharma@gmail.com>
2021-04-22 11:35:47 +0530

    adding case study section

--- components/sections/customers.js
+++ components/sections/customers.js
@@ -29,6 +29,37 @@ function TempShowcase() {
     return <Image src="/assets/showcase-banner.png" />;
 }
 
+function CaseStory() {
+    return (
+        <Flex justifyContent="center">
+            <HStack
+                justifyContent="center"
+                bg="white"
+                spacing="12"
+                p="4"
+                maxW="450px"
+                borderRadius="6px"
+                mt="-136px"
+                mb="64px"
+                boxShadow="0px 30px 60px rgb(0 0 0 / 12%)"
+            >
+                <Image src="/assets/hulu.svg" w="24%" />
+                <Button
+                    mt="32px"
+                    px="56px"
+                    h="44px"
+                    color="white"
+                    bg="#0070f3"
+                    _hover={{ background: 'rgba(0,118,255,0.9)' }}
+                    boxShadow="0 6px 20px rgb(0 118 255 / 23%)"
+                >
+                    Read Case Study
+                </Button>
+            </HStack>
+        </Flex>
+    );
+}
+
 export default function CustomerSection() {
     return (
         <Box
@@ -38,6 +69,7 @@ export default function CustomerSection() {
             borderTop="1px solid #eaeaea"
             borderBottom="1px solid #eaeaea"
         >
+            <CaseStory />
             <Heading textAlign="center" fontSize="32px">
                 Who’s Using Next.js
             </Heading>
