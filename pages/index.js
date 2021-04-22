ec79ca7bc60b21faa6da39c6ff1b981437e4d44a
jai-sharma <sendtojsharma@gmail.com>
2021-04-22 08:04:09 +0530

    adding customer section

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Banner from '../components/banner';
 import Improvements from '../components/improvements';
 import Navbar from '../components/navbar';
+import CustomerSection from '../components/sections/customers';
 import FeatureSection from '../components/sections/features';
 import HeroSection from '../components/sections/hero';
 
@@ -13,11 +14,12 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Banner/>
-            <Navbar/>
-            <Improvements/>
-            <HeroSection/>
-            <FeatureSection/>
+            <Banner />
+            <Navbar />
+            <Improvements />
+            <HeroSection />
+            <FeatureSection />
+            <CustomerSection />
         </>
     );
 }
